e4d9c9c0295f83b75f8f70f17b6ab1ac240576c4
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-03 20:35:43 +0100

    Done lesson 15

---
+++ next-course/pages/index.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hell NextJS</h1>
+    </div>
+  );
+}
