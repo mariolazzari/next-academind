8cafc8ca5ea2d0e050dde7a8754325c6bdf29dbf
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-04 18:17:07 +0100

    Done lesson 29

---
+++ react-course/src/components/Todo.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
+const Todo = ({ text }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const deleteHandler = e => {
+    setModalIsOpen(false);
+  };
+
+  const closeModalHandler = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="card">
+      <h2>{text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={deleteHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+    </div>
+  );
+};
+
+export default Todo;
