d02671512ede78e6750df90c62daa12ecb4ce433
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-06 15:06:16 +0100

    Done lesson 41

--- react-router/src/pages/AllMeetups.js
+++ react-router/src/pages/AllMeetups.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-import DUMMY from "../data/dummy-data";
+//import DUMMY from "../data/dummy-data";
 
 const AllMeetups = () => {
+  consy[(loading, setLoading)] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    fetech(
+      "https://react-router-7b31a-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setLoading(false);
+        setMeetups(data);
+      });
+  }, []);
+
   return (
     <section>
       <h1>All meetups</h1>
-      <MeetupList meetups={DUMMY} />
+      <MeetupList meetups={meetups} />
     </section>
   );
 };
