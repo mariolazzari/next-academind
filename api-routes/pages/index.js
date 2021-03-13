c216bc00b1951f11d3c810f54045282e72297095
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-13 17:12:29 +0100

    Done lesson 138

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,7 +1,31 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const mailRef = useRef();
+  const fbRef = useRef();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const data = {
+      email: mailRef.current.value,
+      feedback: fbRef.current.value,
+    };
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" ref={mailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={fbRef}></textarea>
+        </div>
+        <button type="submit">Send feedback</button>
+      </form>
     </div>
   );
 }
