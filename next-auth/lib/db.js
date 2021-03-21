c31a13470a525b76f7d49e83e1889a0d77b880c3
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-21 12:38:00 +0100

    Done lesson 219

---
+++ next-auth/lib/db.js
@@ -0,0 +1,10 @@
+import { MongoClient } from "mongodb";
+
+export async function connectToDatabase() {
+  const client = await MongoClient.connect(
+    // "mongodb+srv://maximilian:ZbJcz3dJ88LSUMlM@cluster0.ntrwp.mongodb.net/auth-demo?retryWrites=true&w=majority"
+    "mongodb://localhost:27017/next-auth"
+  );
+
+  return client;
+}
